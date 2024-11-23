06e992ec4d20dedeeafd3cc5bbcd4f680bbd3854
Rentaro Suzuki <rentajaxa96@gmail.com>
2024-11-23 16:24:21 +0900

    Initial Commit

---
+++ Zukan/Animal.swift
@@ -0,0 +1,20 @@
+//
+//  Animal.swift
+//  Zukan
+//
+//  Created by 鈴木廉太郎 on 2024/11/23.
+//
+
+import UIKit
+
+class Animal {
+    var name: String
+    var details: String
+    var imageName: String
+    
+    init(name: String, details: String,imageName: String) {
+        
+    }
+    
+    
+}
---
+++ Zukan/ViewController.swift
@@ -0,0 +1,30 @@
+//
+//  ViewController.swift
+//  Zukan
+//
+//  Created by 鈴木廉太郎 on 2024/11/23.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+    
+    @IBOutlet var nameLabel: UILabel!
+    @IBOutlet var detailsLabel: UILabel!
+    @IBOutlet var imageView: UIImageView!
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+    
+    @IBAction func next() {
+        
+    }
+    @IBAction func back() {
+        
+    }
+
+
+}
+
